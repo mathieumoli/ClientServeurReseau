61dc9541d7c02e7d89b6ba15c4b7541ad310d66a
Mathieu Molinengo <mathieu.moli@hotmail.com>
2015-11-04 11:31:04 +0100

    Commit int i;

--- src/main.java
+++ src/main.java
@@ -2,7 +2,9 @@
  * Created by user on 04/11/15.
  */
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+        int i;
         System.out.println("hello");
     }
 }
